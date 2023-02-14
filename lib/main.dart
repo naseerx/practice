9da7eb16550e7883150f8f8129c53639413e28b6
Muhammad Naseer <muhammadnaseerkhan1122@gmail.com>
2023-02-14 21:42:31 +0500

    webview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practice/Screnns/home_screen.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,13 +14,50 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Practice',
       theme: ThemeData(
-
         primarySwatch: Colors.deepPurple,
       ),
-      home: const HomeScreen(),
+      home: const TestingPage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
+class TestingPage extends StatefulWidget {
+  const TestingPage({Key? key}) : super(key: key);
 
+  @override
+  State<TestingPage> createState() => _TestingPageState();
+}
+
+class _TestingPageState extends State<TestingPage> {
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://flutter.dev')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://www.zalmisuppliers.com/'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WEBVIEW TESTING'),
+      ),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
