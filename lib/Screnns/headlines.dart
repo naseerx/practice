3d5270cc37758f7798a0d71dc07d3cb8717062f0
Muhammad Naseer <muhammadnaseerkhan1122@gmail.com>
2023-02-26 22:43:24 +0500

    webview

--- lib/Screnns/headlines.dart
+++ lib/Screnns/headlines.dart
@@ -43,135 +43,19 @@ class _HeadlinesScreenState extends State<HeadlinesScreen> {
       }
     }
   }
-  // logout() {
-  //   return showDialog(
-  //     barrierDismissible: false,
-  //     context: context,
-  //     builder: (context) {
-  //       return BackdropFilter(
-  //         filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-  //         child: AlertDialog(
-  //           // backgroundColor: gPrimaryColor,
-  //           shape: RoundedRectangleBorder(
-  //             borderRadius: BorderRadius.circular(20),
-  //           ),
-  //           title: Column(
-  //             children: const [
-  //               Padding(
-  //                 padding: EdgeInsets.all(8.0),
-  //                 child: Text(
-  //                   'Confirmation !',
-  //                   style: TextStyle(color: gWhite),
-  //                 ),
-  //               ),
-  //               SizedBox(
-  //                 height: 15,
-  //               ),
-  //               DelayedDisplay(
-  //                 delay: Duration(microseconds: 400),
-  //                 child: Padding(
-  //                   padding: EdgeInsets.all(8.0),
-  //                   child: Text(
-  //                     'Are you sure to Quit the App',
-  //                     style: TextStyle(color: gWhite),
-  //                   ),
-  //                 ),
-  //               )
-  //             ],
-  //           ),
-  //           // content: Text('We hate to see you leave...'),
-  //           actions: [
-  //             Row(
-  //               children: [
-  //                 const SizedBox(
-  //                   width: 15,
-  //                 ),
-  //                 Expanded(
-  //                   child: DelayedDisplay(
-  //                     delay: const Duration(milliseconds: 800),
-  //                     slidingBeginOffset: const Offset(0.35, 0.0),
-  //                     child: ElevatedButton(
-  //                       style: ElevatedButton.styleFrom(
-  //                         backgroundColor: gGreenButton,
-  //                         shape: RoundedRectangleBorder(
-  //                           borderRadius: BorderRadius.circular(30),
-  //                         ),
-  //                       ),
-  //                       child: const Text(
-  //                         'No',
-  //                         style: TextStyle(
-  //                           fontSize: 20,
-  //                           fontWeight: FontWeight.bold,
-  //                         ),
-  //                       ),
-  //                       onPressed: () {
-  //                         Navigator.of(context).pop(false);
-  //                       },
-  //                     ),
-  //                   ),
-  //                 ),
-  //                 const SizedBox(
-  //                   width: 15,
-  //                 ),
-  //                 Expanded(
-  //                   child: DelayedDisplay(
-  //                     delay: const Duration(milliseconds: 1200),
-  //                     slidingBeginOffset: const Offset(0.35, 0.0),
-  //                     child: ElevatedButton(
-  //                       style: ElevatedButton.styleFrom(
-  //                         backgroundColor: Colors.red,
-  //                         shape: RoundedRectangleBorder(
-  //                           borderRadius: BorderRadius.circular(30),
-  //                         ),
-  //                       ),
-  //                       child: const Text(
-  //                         'Yes',
-  //                         style: TextStyle(
-  //                           fontSize: 20,
-  //                           fontWeight: FontWeight.bold,
-  //                         ),
-  //                       ),
-  //                       onPressed: () {
-  //                         FirebaseAuth.instance.signOut();
-  //                         Get.offAll(() => const HomeScreen(),
-  //                             transition: Transition.downToUp,
-  //                             duration: const Duration(seconds: 1));
-  //                         Fluttertoast.showToast(
-  //                             msg: 'Logout Successfully',
-  //                             backgroundColor: gPrimaryColor,
-  //                             textColor: gBlack);
-  //                       },
-  //                     ),
-  //                   ),
-  //                 ),
-  //                 const SizedBox(
-  //                   width: 15,
-  //                 ),
-  //               ],
-  //             ),
-  //           ],
-  //         ),
-  //       );
-  //     },
-  //   );
-  //
-  // }
   @override
   void initState() {
     super.initState();
     _streamController = StreamController();
     stream = _streamController.stream.asBroadcastStream();
     getProfileApi();
-    if (kDebugMode) {
-      print('ok');
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('NEWS SOURCE'),
+        title: const Text('NEWS HEADLINES'),
         centerTitle: true,
         toolbarHeight: 80,
       ),
