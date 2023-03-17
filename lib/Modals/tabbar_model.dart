a56a636156472014e94e3b03befa53786f597917
Muhammad Naseer <muhammadnaseerkhan1122@gmail.com>
2023-03-17 23:35:31 +0500

    done

--- lib/Modals/tabbar_model.dart
+++ lib/Modals/tabbar_model.dart
@@ -2,8 +2,6 @@ class RentalsModel {
   late String name;
   late String image;
   late String status;
-  late String lNumber;
-  late String carName;
   late String eDate;
   late String sDate;
 
@@ -13,8 +11,6 @@ class RentalsModel {
     required this.status,
     required this.sDate,
     required this.eDate,
-    required this.carName,
-    required this.lNumber,
   });
 
   // Json to Dart Object
@@ -22,12 +18,10 @@ class RentalsModel {
   factory RentalsModel.fromJSON(Map<String, dynamic> map) {
     return RentalsModel(
       name: map['name'],
-      carName: map['carName'],
       eDate: map['eDate'],
       sDate: map['sDate'],
       status: map['status'],
       image: map['image'],
-      lNumber: map['lNumber'],
     );
   }
 }
